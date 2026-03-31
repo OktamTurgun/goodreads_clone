45b0961f754466c0d9ba05c16ff3ebba21e9f5ab
Oktam Turgunov <uktamturgunov30@gmail.com>
2026-03-31 17:38:14 +0500

    feat: add placeholders and fix form widgets

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,13 +3,52 @@ from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
 class RegisterForm(UserCreationForm):
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(
+        required=True,
+        widget=forms.EmailInput(attrs={
+            'placeholder': 'example@gmail.com',
+            'autocomplete': 'off',
+            'class': 'form-control',
+        })
+    )
 
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'placeholder': 'Enter your username',
+                'autocomplete': 'off',
+                'class': 'form-control',
+            }),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['password1'].widget.attrs.update({
+            'placeholder': 'Enter your password',
+            'autocomplete': 'new-password',
+            'class': 'form-control',
+        })
+        self.fields['password2'].widget.attrs.update({
+            'placeholder': 'Confirm your password',
+            'autocomplete': 'new-password',
+            'class': 'form-control',
+        })
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField()
-    password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={
+            'placeholder': 'Enter your username',
+            'autocomplete': 'off',
+            'class': 'form-control',
+        })
+    )
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'placeholder': 'Enter your password',
+            'autocomplete': 'off',
+            'class': 'form-control',
+        })
+    )
